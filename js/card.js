8dd55b3627f0fdf3bfdcd34595f271bf2d319947
Reece Donovan <reecedonovan99@gmail.com>
2020-11-20 23:55:03 +0000

    Added colour variation to js for cards, bit more styling

--- js/card.js
+++ js/card.js
@@ -5,7 +5,8 @@ export default function constructCard(item_num, testimonial_item) {
    * @param  {json item} testimonial_item [testimonial item parsed from json. Contains "name", "position", etc]
    * @return {html string} [returns raw html of filled card template]
    */
-
+  // #33c3f0
+  let hsl = `hsl(${194 + parseInt(item_num * 50)}, 56%, 57%)`;
   return `
   <div id="t-${item_num.toString()}" class="t-card">
     <div class="t-card-circle">
@@ -14,13 +15,15 @@ export default function constructCard(item_num, testimonial_item) {
         }" class="t-card-image"alt="Alumni Image"/>
     </div>
     <div class="t-card-header">
-        <span class="t-card-header-name">${testimonial_item.name.toUpperCase()}</span>
-        <span class="t-card-header-position">${testimonial_item.position.toUpperCase()}</span>
-        <span class="t-card-header-year">${
-          testimonial_item.years_active.end
-        }</span>
+      <ul>
+          <h2 class="t-card-header-name section-title" style="color:${hsl}">${testimonial_item.name.toUpperCase()}</h2>
+          <h2 class="t-card-header-position section-title" style="color:${hsl}">${testimonial_item.position.toUpperCase()}</h2>
+          <h2 class="t-card-header-year section-title" style="color:${hsl}">${
+    testimonial_item.years_active.end
+  }</h2>
+      </ul>
     </div>
-    <div class="t-card-body">
+    <div class="t-card-body" style="background-color:${hsl}">
         <p class="t-card-body-text">${testimonial_item.testimonial}</p>
     </div>
   </div>
--- js/config.js
+++
@@ -1,4 +0,0 @@
-const GITHUB_TESTIMONIAL_URL =
-  "https://raw.githubusercontent.com/UCCNetsoc/alumni/main/testimonials.json";
-
-export default GITHUB_TESTIMONIAL_URL;
