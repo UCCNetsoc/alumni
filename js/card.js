afc104b0a2aa0417a479a91a04f5494302fda930
Reece Donovan <reecedonovan99@gmail.com>
2020-11-21 21:11:06 +0000

    Added logic for card gradients, adjusted styling on desktop and started on faded banner images

--- js/card.js
+++ js/card.js
@@ -5,26 +5,34 @@ export default function constructCard(item_num, testimonial_item) {
    * @param  {json item} testimonial_item [testimonial item parsed from json. Contains "name", "position", etc]
    * @return {html string} [returns raw html of filled card template]
    */
-  // #33c3f0
-  let hsl = `hsl(${194 + parseInt(item_num * 50)}, 56%, 57%)`;
+  let hsl = 194 + parseInt(item_num * 40);
+  let banner_image = "./img/committee-19.jpg";
+  if (testimonial_item.banner_image.length > 3) {
+    banner_image = testimonial_item.banner_image;
+  }
   return `
   <div id="t-${item_num.toString()}" class="t-card">
     <div class="t-card-circle">
         <img src="${
-          testimonial_item.image
-        }" class="t-card-image"alt="Alumni Image"/>
+          testimonial_item.profile_image
+        }" class="t-card-alumni-image"alt="Alumni Image"/>
     </div>
     <div class="t-card-header">
       <ul>
-          <h2 class="t-card-header-name section-title" style="color:${hsl}">${testimonial_item.name.toUpperCase()}</h2>
-          <h2 class="t-card-header-position section-title" style="color:${hsl}">${testimonial_item.position.toUpperCase()}</h2>
-          <h2 class="t-card-header-year section-title" style="color:${hsl}">${
+          <h2 class="t-card-header-name section-title" style="color:hsl(${hsl}, 56%, 57%)">${testimonial_item.name.toUpperCase()}</h2>
+          <h2 class="t-card-header-position section-title" style="color:hsl(${hsl}, 56%, 57%)">${testimonial_item.position.toUpperCase()}</h2>
+          <h2 class="t-card-header-year section-title" style="color:hsl(${hsl}, 56%, 57%)">${
     testimonial_item.years_active.end
   }</h2>
       </ul>
     </div>
-    <div class="t-card-body" style="background-color:${hsl}">
+    <div class="t-card-body" style="background:linear-gradient(hsl(${hsl}deg 56% 57%), hsl(${
+    hsl + 40
+  }deg 56% 57%))">
+      <section>
         <p class="t-card-body-text">${testimonial_item.testimonial}</p>
+      </section>
+      <div class="t-card-body-image" style="background-image:url(${banner_image})"></div>
     </div>
   </div>
   `;
